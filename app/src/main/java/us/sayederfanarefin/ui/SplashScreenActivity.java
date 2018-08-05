f3a6e2e4e302517dcdab6f8e81e5963eb58d9091
Sayed Erfan Arefin <erfan@Tasnims-MacBook-Pro.local>
2018-08-05 18:48:33 +0600

    modifying splash screen

--- app/src/main/java/us/sayederfanarefin/ui/SplashScreenActivity.java
+++ app/src/main/java/us/sayederfanarefin/ui/SplashScreenActivity.java
@@ -9,23 +9,24 @@ import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Fullscreen;
 
-@EActivity(R.layout.content_splash_screen)
-@Fullscreen
+//@EActivity(R.layout.content_splash_screen)
+//@Fullscreen
 public class SplashScreenActivity extends CoreActivity {
 
-    @AfterViews
-    void afterViews() {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                openFirstActivity();
-                finish();
-            }
-        }, 1000);
-    }
+//    @AfterViews
+//    void afterViews() {
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                openFirstActivity();
+//                finish();
+//            }
+//        }, 1000);
+//    }
+//
+//    private void openFirstActivity() {
+//        FirstActivity_.intent(this).start();
+//    }
 
-    private void openFirstActivity() {
-        FirstActivity_.intent(this).start();
-    }
 
 }
